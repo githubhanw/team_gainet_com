62340c8877cafa3c7528df3f500952d6978f9454
Qyb <Qyb@192.168.67.194>
2018-10-15 15:06:28 +0800

    项目列表分页换模板

--- src/main/java/com/zzidc/team/controller/TeamProjectController.java
+++ src/main/java/com/zzidc/team/controller/TeamProjectController.java
@@ -59,6 +59,7 @@ public class TeamProjectController extends GiantBaseController {
 		if("".equals(GiantUtil.stringOf(mvm.get("orderColumn")))){
 			mvm.put("orderColumn", "id");
 			mvm.put("orderByValue", "DESC");
+			mvm.put("currentPage", "1");
 		}
 		if("".equals(GiantUtil.stringOf(mvm.get("type")))){
 			mvm.put("type", "1");
@@ -73,7 +74,7 @@ public class TeamProjectController extends GiantBaseController {
 		conditionPage.setPageSize(GiantUtil.intOf(mvm.get("pageSize"), 15));
 		conditionPage.setOrderColumn(GiantUtil.stringOf(mvm.get("orderColumn")));
 		pageList = teamProjectService.getPageList(conditionPage);
-		requestURL = "team/project/index?type=" + mvm.get("type") + "&currentPage=" + pageList.getCurrentPage() + "&pageSize=" + pageList.getPageSize() + "&search=" + mvm.get("search");
+		requestURL = "team/project/index";
 		pageList.setDesAction(requestURL);
 		model.addAttribute("members", teamProjectService.getAllMember());
 		model.addAttribute("pageList", pageList);

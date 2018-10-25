d328879f273a7136d00d54f399204fa57b28aca6
张玉兵 <zhangyubing@zzidc.com>
2018-10-25 21:15:22 +0800

    --

--- src/main/java/com/zzidc/log/LogModule.java
+++ src/main/java/com/zzidc/log/LogModule.java
@@ -11,7 +11,8 @@ public enum LogModule {
 	TASK("task"),
 	NEED("need"),
 	TEST("test"),
-	BUG("bug");
+	BUG("bug"),
+	RESULT("result");
 	
 	private LogModule(String value) {
 		this.value = value;
